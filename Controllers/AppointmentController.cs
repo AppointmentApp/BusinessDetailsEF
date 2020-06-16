f6d7cc6b9f981616e65853ec8f9e65b1d4378c4f
Shubham Jagtap <shubhamjagtap528@gmail.com>
2020-06-16 17:16:39 +0530

    Removed unnecessary usings

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using BusinessDetailsEF.Adapter;
 using BusinessDetailsEF.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 
 namespace BusinessDetailsEF.Controllers
 {
@@ -26,16 +18,7 @@ namespace BusinessDetailsEF.Controllers
             try
             {
                 var appointments = appAdapter.getallappointments();
-                //var appointments = auc.Appointments.Select(p => new AppointmentsEntity()
-                //{AappointmentId=p.appointmentId,
-                //ABusiness_Id=p.Business_Id,
-                //AName=p.Name,
-                //Aaddress=p.address,
-                //Adate1=p.date1,
-                //AtimeSlot=p.timeSlot,
-                //AContact=p.Contact,
-                //Astatus=p.status
-                //}).ToList();
+          
                 return Ok(appointments);
             }
             catch 
@@ -51,16 +34,7 @@ namespace BusinessDetailsEF.Controllers
             try
             {
                 var appointments = appAdapter.getallappointmentbyid(id);
-                //var appointments = auc.Appointments.Where(p=>p.appointmentId==id).Select(p => new AppointmentsEntity()
-                //{
-
-                //    AName = p.Name,
-                //    Aaddress = p.address,
-                //    Adate1 = p.date1,
-                //    AtimeSlot = p.timeSlot,
-                //   AContact = p.Contact,
-                //    Astatus = p.status
-                //}).ToList();
+     
                 return Ok(appointments);
             }
             catch
@@ -76,17 +50,7 @@ namespace BusinessDetailsEF.Controllers
             try
             {
                 var appoints = appAdapter.addappointments(appointments);
-                //var appoints = new Appointments()
-                //{
-                //    Name = appointments.AName,
-                //    address = appointments.Aaddress,
-                //    date1 = appointments.Adate1,
-                //    timeSlot = appointments.AtimeSlot,
-                //    Contact = appointments.AContact,
-                //    status = appointments.Astatus
-                //};
-                //auc.Appointments.Add(appoints);
-                //auc.SaveChanges();
+              
                 return Ok(appoints);
             }
             catch
@@ -105,13 +69,7 @@ namespace BusinessDetailsEF.Controllers
             try
             {
                 var ad = appAdapter.updatestatus(appentity, id);
-                //    if (ad != null)
-                //    {
-
-                //        ad.status = appentity.Astatus;
-                //    }
-                //    auc.Appointments.Update(ad);
-                //    auc.SaveChanges();
+     
                 return Ok(ad);
             }
             catch 
@@ -128,9 +86,7 @@ namespace BusinessDetailsEF.Controllers
             try
             {
                 var appointment = appAdapter.deleteappointment(id);
-                //var appointment = auc.Appointments.Find(id);
-                //auc.Appointments.Remove(appointment);
-                //auc.SaveChanges();
+                
                 return Ok(appointment);
             }
             catch
